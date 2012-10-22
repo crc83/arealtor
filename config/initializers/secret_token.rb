cbb509dbab9842167eb6bf6e55a0cadd5e9e470f
Сергій Белей <sbelei@localhost.localdomain>
2012-10-22 18:04:27 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ARealtor::Application.config.secret_token = 'e4c3e78e3a64e1d5548482ad5a7c7c5141748a53ec193b58e07ccd99ff952ef45a91f97bd38a8bf261fb6ccbf0df96eabf31f58d85b6cc3a556af178a1c645bc'
