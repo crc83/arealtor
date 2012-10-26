5c0bc1818262ccd2b5476009b7d8e6bce1cd5077
Sergiy Beley <sergiy.beley@gmail.com>
2012-10-26 17:40:43 +0300

    Fixed URL of home page location in spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit 'static_pages/home'
+      visit '/static_pages/home'
       page.should have_content('Sample App')
     end
   end
