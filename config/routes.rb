46aa42a7f4df868292823cfcccaeba83d043bdf6
Sergiy Beley <sergiy.beley@gmail.com>
2012-10-29 12:01:13 +0200

    Add about page and spec for it

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ARealtor::Application.routes.draw do
   get "static_pages/home"
-
   get "static_pages/help"
+  get "static_pages/about"
 
   resources :microposts
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -15,4 +15,11 @@ describe "StaticPages" do
     end
   end
 
+  describe "About page" do
+    it "should have content 'About'" do
+      visit '/static_pages/about'
+      page.should have_content('About')
+    end
+  end
+
 end
