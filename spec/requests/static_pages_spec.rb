30b83125fe06c8bd1f27c51ddcd92dada7908699
Sergiy Beley <sergiy.beley@gmail.com>
2012-10-31 17:15:40 +0200

    Add before and subject shortcuts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,52 +1,53 @@
 require 'spec_helper'
 
 describe "Static pages: " do
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
+    
     it "should have the content 'Home'" do
-      visit root_path
-      page.should have_selector('h1',:text => 'Home')
+      should have_selector('h1',:text => 'Home')
     end
 
     it "should have the title 'Home'" do
-      visit root_path
-      page.should have_selector('title',:text => "ARealtor | Home")
+      should have_selector('title',:text => "ARealtor | Home")
     end
   end
 
   describe "Help page" do
+    before { visit help_path }
+
     it "should have the content 'Help'" do
-      visit help_path
-      page.should have_selector('h1',:text =>'Help')
+      should have_selector('h1',:text =>'Help')
     end
 
     it "should have the title 'Help'" do
-      visit help_path
-      page.should have_selector('title', :text => "Help")
+      should have_selector('title', :text => "Help")
     end
   end
 
   describe "About page" do
+    before { visit about_path }
+
     it "should have content 'About'" do
-      visit about_path
-      page.should have_selector('h1', :text => 'About')
+      should have_selector('h1', :text => 'About')
     end
 
     it "should have title 'About'" do
-      visit about_path
-      page.should have_selector('title', :text => 'About')
+      should have_selector('title', :text => 'About')
     end
   end
 
   describe "Contact page" do
+    before { visit contact_path }
     
     it "should have h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1', text: 'Contact')
+      should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit contact_path
-      page.should have_selector('title', text: 'Contact')
+      should have_selector('title', text: 'Contact')
     end
 
   end
