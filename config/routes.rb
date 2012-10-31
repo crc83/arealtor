e59687dcbd012b4ad990f9c6f9e24c7f5ef82096
Sergiy Beley <sergiy.beley@gmail.com>
2012-10-31 14:47:19 +0200

    Layout links ajusted in code and tests

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ ARealtor::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+  match '/help',  to: 'static_pages#help'
+  match '/about',  to: 'static_pages#about'
+  match '/contact',  to: 'static_pages#contact'
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
@@ -58,6 +61,7 @@ ARealtor::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root to: 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,38 +1,38 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages: " do
   describe "Home page" do
     it "should have the content 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('h1',:text => 'Home')
     end
 
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title',:text => "ARealtor | Home")
     end
   end
 
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('h1',:text =>'Help')
     end
 
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       page.should have_selector('title', :text => "Help")
     end
   end
 
   describe "About page" do
     it "should have content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('h1', :text => 'About')
     end
 
     it "should have title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       page.should have_selector('title', :text => 'About')
     end
   end
@@ -40,13 +40,13 @@ describe "StaticPages" do
   describe "Contact page" do
     
     it "should have h1 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       page.should have_selector('h1', text: 'Contact')
     end
 
     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('h1', text: 'Contact')
+      visit contact_path
+      page.should have_selector('title', text: 'Contact')
     end
 
   end
