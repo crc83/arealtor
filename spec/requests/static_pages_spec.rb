6f115f5f9260a964d1afbb6df5581c2ad367cd14
Sergiy Beley <sergiy.beley@gmail.com>
2012-10-31 17:32:29 +0200

    Remove do clauses

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,49 +6,28 @@ describe "Static pages: " do
   describe "Home page" do
     before { visit root_path }
     
-    it "should have the content 'Home'" do
-      should have_selector('h1',:text => 'Home')
-    end
-
-    it "should have the title 'Home'" do
-      should have_selector('title',:text => "ARealtor | Home")
-    end
+    it { should have_selector('h1',:text => 'Home') }
+    it { should have_selector('title',:text => "ARealtor | Home") }
   end
 
   describe "Help page" do
     before { visit help_path }
 
-    it "should have the content 'Help'" do
-      should have_selector('h1',:text =>'Help')
-    end
-
-    it "should have the title 'Help'" do
-      should have_selector('title', :text => "Help")
-    end
+    it { should have_selector('h1',:text =>'Help') }
+    it { should have_selector('title', :text => "Help") }
   end
 
   describe "About page" do
     before { visit about_path }
 
-    it "should have content 'About'" do
-      should have_selector('h1', :text => 'About')
-    end
-
-    it "should have title 'About'" do
-      should have_selector('title', :text => 'About')
-    end
+    it { should have_selector('h1', :text => 'About') }
+    it { should have_selector('title', :text => 'About') }
   end
 
   describe "Contact page" do
     before { visit contact_path }
     
-    it "should have h1 'Contact'" do
-      should have_selector('h1', text: 'Contact')
-    end
-
-    it "should have the title 'Contact'" do
-      should have_selector('title', text: 'Contact')
-    end
-
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('title', text: 'Contact') }
   end
 end
