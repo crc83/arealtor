69e188b74459b0ba06573d90f171af20b2d7a3ab
Sergiy Beley <sergiy.beley@gmail.com>
2012-11-14 12:46:30 +0200

    Add remember token field to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121107114151) do
+ActiveRecord::Schema.define(:version => 20121114100634) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(:version => 20121107114151) do
     t.string   "password_digest"
     t.string   "password"
     t.string   "password_confirmation"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
