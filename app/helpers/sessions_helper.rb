78e9dcd115ae62f000337ae0d2a54a272f12d85c
Sergiy Beley <sergiy.beley@gmail.com>
2012-11-15 10:52:30 +0200

    Menu finished

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,12 +13,12 @@ module SessionsHelper
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
-    def sign_in(user)
-      cookies.permanent[:remember_token] = user.remember_token
-      self.current_user = user
-    end
+  def sign_in(user)
+    cookies.permanent[:remember_token] = user.remember_token
+    self.current_user = user
+  end
 
-    def signed_in?
-      !current_user.nil?
-    end
+  def signed_in?
+    !current_user.nil?
+  end
 end
