65ac1e7798a1eba134015728d856a8cfa3275983
Sergiy Beley <sergiy.beley@gmail.com>
2012-11-19 23:33:56 +0200

    4nd exercise : Use 'signed in' helper

--- spec/requests/authentification_pages_spec.rb
+++ spec/requests/authentification_pages_spec.rb
@@ -22,11 +22,7 @@ describe "Authentification" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
-      end
+      before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
       it { should have_link('Profile', href: user_path(user)) }
@@ -100,10 +96,8 @@ describe "Authentification" do
     describe "when attempting to visit a protected page" do
       let(:user) { FactoryGirl.create(:user) }
       before do
-        visit edit_user_path(user)
-        fill_in "Email",    with: user.email
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+        visit edit_user_path(user) 
+        sign_in user 
       end
       
       describe "after signing in" do
